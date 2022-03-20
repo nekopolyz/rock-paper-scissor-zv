4dcd7ee5c9febd763932c87a3d96dc2c92df2d5c
Lan <minenie@gmail.com>
2022-03-20 14:56:56 -0700

    loop done

--- main.js
+++ main.js
@@ -1,7 +1,65 @@
 function computerPlay(){
     let computer = ["rock","paper","scissor"];
     let randomize = computer[Math.floor(Math.random() * computer.length)];
-    return randomize
+    return randomize;
 }
 
 computerPlay()
+console.log(computerPlay());
+
+
+
+function playRound(playerSelection,computerSelection){
+    if (playerSelection === computerSelection){
+        return "tie";
+    }
+
+    if (playerSelection == "rock" && computerSelection == "paper"){
+        return "You lost";
+    }
+
+    if (playerSelection == "rock" && computerSelection == "scissor"){
+        return "You win";
+    }
+
+    if (playerSelection == "paper" && computerSelection == "rock"){
+        return "You win";
+    }
+
+    if (playerSelection == "paper" && computerSelection == "scissor"){
+        return "You lost";
+    }
+
+    if (playerSelection == "scissor" && computerSelection == "rock"){
+        return "You lost";
+    }
+
+    if (playerSelection == "scissor" && computerSelection == "paper"){
+        return "You win";
+    }
+
+}
+
+
+
+const playerSelection = prompt("Choose").toLowerCase()
+
+const computerSelection = computerPlay()
+
+
+
+
+console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+    for (let i =0; i<5; i++){
+        const playerSelection = prompt("Choose").toLowerCase()
+        const computerSelection = computerPlay()
+        console.log(playRound(playerSelection,computerSelection));
+
+    }
+}
+
+
+game()
+
